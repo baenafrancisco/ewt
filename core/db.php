5b1df470ebd773d30fa27959d2327292044ddc57
Francisco Baena <soy@franbaena.com>
2015-02-19 14:37:02 +0000

    Started EWT assessment

---
+++ core/db.php
@@ -0,0 +1,8 @@
+<?php
+	
+	// Database credentials
+	const DB_HOST = "localhost";
+	const DB_NAME = "fbaenarodriguez";
+	const DB_USERNAME = "fbaenarodriguez";
+	const DB_PASSWORD = "sheethoh";
+?>
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>EWT Assessment</title>
+</head>
+<body>
+
+<?php
+echo "Hello world!";
+?>
+
+</body>
+</html>
